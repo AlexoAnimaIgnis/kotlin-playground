143ae6c448b1880bfd62ce0112a29543d4106ce1
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-08-22 19:46:17 +0800

    kotlin nullable and safety operator

---
+++ src/main/kotlin/com/kotlinplayground/nulls/ExploreNulls.kt
@@ -0,0 +1,30 @@
+package com.kotlinplayground.nulls
+
+data class Movie(
+    val id: Int?,
+    val name: String
+)
+
+fun saveMovie(movie: Movie) : Movie{
+    return movie.copy(id =1)
+}
+
+
+fun main() {
+    var nameNullable : String? = null
+    println("value is ${nameNullable?.length}") // ? - safe call operators , meaning execute only if the variable is not null
+//    if (nameNullable != null) {
+//        println("value is ${nameNullable.length}")
+//    }
+
+    nameNullable = "Alexo"
+    val length = nameNullable?.length
+    println("Length: $length")
+    println("Value is: $nameNullable")
+
+    var name: String = "Alexo"
+    val movie = Movie(null, "Avengers")
+    val savedMovie = saveMovie(movie)
+    println("SavedMoview : $savedMovie")
+}
+
