202b1a7ae057c680c0d6fe5cba04a94b5613010b
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-08-29 11:37:58 +0800

    kotlin high order function

--- src/main/kotlin/com/kotlinplayground/lambda/ExploreLambda.kt
+++ src/main/kotlin/com/kotlinplayground/lambda/ExploreLambda.kt
@@ -1,5 +1,11 @@
 package com.kotlinplayground.lambda
 
+
+// high-ordered function -> a function that accepts another function
+fun calculate(x: Int, y: Int, op: (x: Int, y: Int) -> Int): Int {
+    return op(x, y)
+}
+
 fun main() {
     val add = { x: Int -> x + x } // similar to fun add(x:Int) = x+x
 
@@ -13,6 +19,15 @@ fun main() {
         println("multiply $x * $y")
         x * y  // last statement will be returned in multiline lambda
     }
-    val result = multiplyLambda(2,3)
+
+    val result = multiplyLambda(2, 3)
     println("multiple result : $result")
+
+    //calculate(2, 3, {a, b -> a *b})
+    val highOrderResult1 = calculate(2, 3) { a, b -> a * b } // if lambda is the last argument of the method, we can put it outside
+    println("Result is: " + highOrderResult1)
+
+
+    val highOrderResult2 = calculate(2, 3, { a, b -> a + b })
+    println("Result is: " + highOrderResult2)
 }
