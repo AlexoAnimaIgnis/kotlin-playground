da20bbac7e3b368ca1fda34232ca23af68235b7c
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-08-29 12:17:46 +0800

    kotlin explore hashmap

--- src/main/kotlin/com/kotlinplayground/collections/ExploreColMethods.kt
+++ src/main/kotlin/com/kotlinplayground/collections/ExploreColMethods.kt
@@ -14,23 +14,49 @@ fun main() {
     //exploreFilter(courseList, designPredicate)
     //exploreMap(courseList, designPredicate)
 
-    val list = listOf(listOf(1,2,3), listOf(4,5,6))
-    val mapList = list.map { outerList ->
-        outerList.map {
-            it.toDouble()
-        }
-    }
-    println("MapResult: $mapList")
+//    val list = listOf(listOf(1,2,3), listOf(4,5,6))
+//    val mapList = list.map { outerList ->
+//        outerList.map {
+//            it.toDouble()
+//        }
+//    }
+//    println("MapResult: $mapList")
+//
+//    val flatMapResult = list.flatMap { outerList ->
+//        outerList.map {
+//            it.toDouble()
+//        }
+//    }
+//    println("flatMapResult: $flatMapResult")
+//
+//    val courses = exploreFlatMap(courseList, KAFKA)
+//    println("Courses: $courses")
 
-    val flatMapResult = list.flatMap { outerList ->
-        outerList.map {
-            it.toDouble()
-        }
-    }
-    println("flatMapResult: $flatMapResult")
+    exploreHashMap()
+}
 
-    val courses = exploreFlatMap(courseList, KAFKA)
-    println("Courses: $courses")
+fun exploreHashMap() {
+    val nameAgeMutableMap = mutableMapOf("Alexo" to 33, "Scooby" to 34)
+    nameAgeMutableMap
+        .forEach { (t, u) ->
+            println("Key : $t and the value is $u")
+        }
+
+    val value = nameAgeMutableMap.getOrDefault("Dilip1") { "abc" } // assign a default value if key is missing
+    println("Value is $value")
+
+    val ispresent = nameAgeMutableMap.containsKey("abc")
+    println("isPresent : $ispresent")
+
+    val filteredKeyMap = nameAgeMutableMap
+        .filterKeys { it.length > 5 }
+        .map { it.key.uppercase() }
+    println("filteredMap: $filteredKeyMap")
+
+    val highestAge = nameAgeMutableMap
+        .maxByOrNull { it.value }
+
+    println("Max age:  $highestAge")
 }
 
 fun exploreFlatMap(courseList: MutableList<Course>, kafka: String): List<String> {
@@ -50,7 +76,7 @@ fun exploreFlatMap(courseList: MutableList<Course>, kafka: String): List<String>
 fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
     val courses = courseList
         .filter(predicate)
-        .map { "${it.name} - ${it.category}"  }
+        .map { "${it.name} - ${it.category}" }
         .forEach {
             println(it)
         } // foreach is a terminal operator
